af3b0674362c56403d9213117e1a00dd1b75fd6d
El Roble <san_lucas2005@yahoo.com>
2021-08-07 20:47:44 -0600

    Remove some unused imports

--- client/src/components/CartItem/index.js
+++ client/src/components/CartItem/index.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import { REMOVE_FROM_CART, UPDATE_CART_QUANTITY } from "../../utils/actions";
 import { idbPromise } from "../../utils/helpers";
 
 const CartItem = ({ item }) => {
 
   const dispatch = useDispatch();
-  const state = useSelector((state) => state);
 
   const removeFromCart = item => {
     dispatch({
--- client/src/utils/GlobalState.js
+++ client/src/utils/GlobalState.js
@@ -8,27 +8,3 @@ export default function StoreProvider(props) {
   // Return the provider component provided by react-redux
   return <Provider store={store} {...props} />;
 }
-
-// import React, { createContext, useContext } from "react";
-// import { useProductReducer } from './reducers'
-
-// const StoreContext = createContext();
-// const { Provider } = StoreContext;
-
-// const StoreProvider = ({ value = [], ...props }) => {
-//   const [state, dispatch] = useProductReducer({
-//     products: [],
-//     cart: [],
-//     cartOpen: false,
-//     categories: [],
-//     currentCategory: '',
-//   });
-
-//   return <Provider value={[state, dispatch]} {...props} />;
-// };
-
-// const useStoreContext = () => {
-//   return useContext(StoreContext);
-// };
-
-// export { StoreProvider, useStoreContext };
